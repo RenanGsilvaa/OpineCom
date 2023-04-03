e485515ccbca322e38bc38f5eb8b78cc9d189a3b
RenanGsilvaa <profissionalrenann@outlook.com>
2023-04-03 20:01:36 -0300

    É isso aí

--- resources/views/Usuarios/index.blade.php
+++ resources/views/Usuarios/index.blade.php
@@ -4,4 +4,6 @@
 <a href="{{route('usuarios.create')}}">Criar um Usuário</a>
 <a href="{{route('opiniao.create')}}">Fazer uma avalação</a>
 <a href="{{route('usuarios.list')}}">Ver usuários cadastrados</a>
+
+
 @endsection
